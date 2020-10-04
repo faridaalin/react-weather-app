a9d6d80bcf682459cf2172336e392de11b3b1487
Farida <farida.alin@yahoo.com>
2020-10-04 14:21:00 +0200

    code refacturing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { ClipLoader } from "react-spinners";
-import GlobalStyle from "./style/base/globalStyles";
-import setDate from "./utils/setDate";
-import calCelcius from "./utils/calCelcius";
-import getTime from "./utils/getTime";
-import setWeatherIcon from "./functions/setWeatherIcon";
-import Theme from "./style/theme/theme";
 import apiConfig from "./utils/apiConfig";
-
-import {
-  Container,
-  SearchField,
-  Time,
-  Header,
-  MyProviderStyled,
-  IconContainer,
-  WeatherInfo,
-  SubHeading,
-  Temperature,
-  MinMaxTemp,
-  SunsetSunrise,
-  Daylength,
-  LoaderContainer,
-} from "./style/container";
+import SearchBox from "./components/SearchBox";
+import WeatherInterface from "./components/WeatherInterface";
 
 function App() {
   const [loader, setLoader] = useState(false);
   const [query, setQuery] = useState(null);
-  const [weather, setWeather] = useState(null);
+  const [weatherData, setWeatherData] = useState({});
 
   const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiConfig.API_KEY}`;
 
@@ -39,7 +18,7 @@ function App() {
       try {
         const res = await fetch(URL);
         const data = await res.json();
-        setWeather(data);
+        setWeatherData(data);
         setLoader(false);
       } catch {
         console.error();
@@ -50,7 +29,7 @@ function App() {
 
   const handleKeypress = (e) => {
     if (e.key === "Enter") {
-      let value = e.target.value;
+      let { value } = e.target;
       if (value.length < 1) {
         return;
       }
@@ -60,86 +39,24 @@ function App() {
   };
 
   if (query) {
-    if (weather && weather.name) {
+    if (weatherData && weatherData.name) {
       return (
-        <>
-          <GlobalStyle />
-          <Theme>
-            <MyProviderStyled>
-              <Container>
-                <SearchField
-                  placeholder="Search by city"
-                  type="text"
-                  onKeyPress={handleKeypress}
-                />
-                <LoaderContainer>
-                  <ClipLoader color="#053532" loading={loader} />
-                </LoaderContainer>
-
-                <Time>{setDate(new Date())}</Time>
-                <Header>
-                  {weather.name}, {weather.sys.country}
-                </Header>
-                <IconContainer>
-                  {setWeatherIcon(weather.weather[0].id)}
-                </IconContainer>
-                <WeatherInfo>
-                  <SubHeading>{weather.weather[0].description}</SubHeading>
-                  <Temperature>{calCelcius(weather.main.temp)}°</Temperature>
-
-                  <MinMaxTemp>
-                    <span>Min {calCelcius(weather.main.temp_max)}°</span>
-                    <span>Max {calCelcius(weather.main.temp_min)}°</span>
-                  </MinMaxTemp>
-                </WeatherInfo>
-
-                <SunsetSunrise className="sunset-sunrise">
-                  <Daylength>
-                    <p>{getTime(weather.sys.sunrise)}</p>
-                    <p>SUNRISE</p>
-                  </Daylength>
-                  <Daylength>
-                    <p>{getTime(weather.sys.sunset)}</p>
-                    <p>SUNSET</p>
-                  </Daylength>
-                </SunsetSunrise>
-              </Container>
-            </MyProviderStyled>
-          </Theme>
-        </>
+        <WeatherInterface
+          handleKeypress={handleKeypress}
+          {...weatherData}
+          loader={loader}
+        />
       );
     } else {
       return (
-        <>
-          <GlobalStyle />
-          <Theme>
-            <Container>
-              <SearchField
-                placeholder="Search by city"
-                type="search"
-                onKeyPress={handleKeypress}
-              />
-              <div>City with the name "{query}"" does not exist.</div>
-            </Container>
-          </Theme>
-        </>
+        <SearchBox
+          message={`City with the name '${query}' does not exist.`}
+          handleKeypress={handleKeypress}
+        />
       );
     }
   } else {
-    return (
-      <>
-        <GlobalStyle />
-        <Theme>
-          <Container>
-            <SearchField
-              placeholder="Search by city"
-              type="search"
-              onKeyPress={handleKeypress}
-            />
-          </Container>
-        </Theme>
-      </>
-    );
+    return <SearchBox message="" handleKeypress={handleKeypress} />;
   }
 }
 
---
+++ src/components/WeatherInterface.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { ClipLoader } from "react-spinners";
+import GlobalStyle from "../style/base/globalStyles";
+import setDate from "../utils/setDate";
+import calCelcius from "../utils/calCelcius";
+import getTime from "../utils/getTime";
+import setWeatherIcon from "../functions/setWeatherIcon";
+import Theme from "../style/theme/theme";
+
+
+import {
+    Container,
+    SearchField,
+    Time,
+    Header,
+    MyProviderStyled,
+    IconContainer,
+    WeatherInfo,
+    SubHeading,
+    Temperature,
+    MinMaxTemp,
+    SunsetSunrise,
+    Daylength,
+    LoaderContainer,
+} from "../style/container";
+
+export default function WeatherInterface({ handleKeypress, loader, ...weatherData }) {
+    return (
+        <>
+            <GlobalStyle />
+            <Theme>
+                <MyProviderStyled>
+                    <Container>
+                        <SearchField
+                            placeholder="Search by city"
+                            type="text"
+                            onKeyPress={handleKeypress}
+                        />
+
+                        <LoaderContainer>
+                            <ClipLoader color="#053532" loading={loader} />
+                        </LoaderContainer>
+
+                        <Time>{setDate(new Date())}</Time>
+                        <Header>
+                            {weatherData.name}, {weatherData.sys.country}
+                        </Header>
+                        <IconContainer>
+                            {setWeatherIcon(weatherData.weather[0].id)}
+                        </IconContainer>
+                        <WeatherInfo>
+                            <SubHeading>{weatherData.weather[0].description}</SubHeading>
+                            <Temperature>{calCelcius(weatherData.main.temp)}°</Temperature>
+
+                            <MinMaxTemp>
+                                <span>Min {calCelcius(weatherData.main.temp_max)}°</span>
+                                <span>Max {calCelcius(weatherData.main.temp_min)}°</span>
+                            </MinMaxTemp>
+                        </WeatherInfo>
+
+                        <SunsetSunrise className="sunset-sunrise">
+                            <Daylength>
+                                <p>{getTime(weatherData.sys.sunrise)}</p>
+                                <p>SUNRISE</p>
+                            </Daylength>
+                            <Daylength>
+                                <p>{getTime(weatherData.sys.sunset)}</p>
+                                <p>SUNSET</p>
+                            </Daylength>
+                        </SunsetSunrise>
+                    </Container>
+                </MyProviderStyled>
+            </Theme>
+        </>
+    )
+}
